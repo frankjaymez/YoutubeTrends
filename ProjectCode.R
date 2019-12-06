387c1f784502d9c07857d7258bc351051f306782
Kevin <vokevin91@gmail.com>
2019-12-06 14:34:59 -0600

    added

--- ProjectCode.R
+++ ProjectCode.R
@@ -9,25 +9,20 @@ youtube <- read.csv(file.choose(), header = TRUE)
 #Randomly select a subset of data
 youtube_sample <- youtube[sample(1:nrow(youtube), 500, replace = FALSE),]
 
-#Predata Processing
+str(youtube_sample)
+
+#Predata Processing. Convert category_id to factor
 youtube_sample$category_id <- as.factor(youtube_sample$category_id)
-
-#Get unique category ID
-unique_id <- youtube[, c(5)]
-unique_id <- unique(unique_id)
-
-#Get overall data type and dim
-glimpse(youtube)
 
 #Get variables likes, dislikes, count, category, and views
 youtube_var <- youtube_sample[, c(5, 8, 9, 10, 11)]
 
+########################
 #Classification Tree
 tree.youtube = tree(category_id ~ ., data = youtube_var)
 summary(tree.youtube)
 plot(tree.youtube)
 text(tree.youtube, pretty = 0)
-tree.youtube
 
 #Test Classification Error
 RNGkind(sample.kind = "Rounding")
@@ -46,8 +41,20 @@ cv.youtube = cv.tree(tree.youtube, FUN = prune.misclass)
 names(cv.youtube)
 cv.youtube
 par(mfrow=c(1,2))
-plot(cv.youtube$)
+plot(cv.youtube$size, cv.youtube$dev, type = "b")
+plot(cv.youtube$k, cv.youtube$dev, type = "b")
+par(mfrow=c(1,1))
+prune.youtube = prune.misclass(tree.youtube, best = 8)
+plot(prune.youtube)
+text(prune.youtube, pretty = 0)
+
+tree.pred = predict(prune.youtube, youtube.test, type = "class")
+table(tree.pred, category.test)
 
 #Random Forest
-rf.youtube <- randomForest(category_id ~., data = trainset, importance = TRUE)
-rf.youtube
+RNGkind(sample.kind = "Rounding")
+set.seed(1)
+rf.youtube = randomForest(category_id ~., data = youtube_var, subset = train, mtry = 3, importance = TRUE)
+importance(rf.youtube)
+varImpPlot(rf.youtube)
+
