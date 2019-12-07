bfd64d4b9bf9649f030cad0e3c72c6fc302a9b44
Kevin <vokevin91@gmail.com>
2019-12-06 19:07:27 -0600

    add

--- ProjectCode.R
+++ ProjectCode.R
@@ -54,7 +54,34 @@ table(tree.pred, category.test)
 #Random Forest
 RNGkind(sample.kind = "Rounding")
 set.seed(1)
-rf.youtube = randomForest(category_id ~., data = youtube_var, subset = train, mtry = 3, importance = TRUE)
+rf.youtube = randomForest(category_id ~., data = youtube_var, subset = train, importance = TRUE)
+
 importance(rf.youtube)
 varImpPlot(rf.youtube)
+
+#Test Random Forest
+for(i in 1:10){
+  train <- sample(nrow(youtube_var), 0.8*nrow(youtube_var), replace = FALSE)
+  trainset <- youtube_var[train,]
+  validset <- youtube_var[-train,]
+  #summary(trainset)
+  #summary(validset)
+  
+  model1 <- randomForest(category_id ~ ., data = trainset, importance = TRUE)
+  
+  #model2 <- randomForest(category_id ~ ., data = trainset, ntree = 500, mtry = 3, importance = TRUE)
+  
+  predTrain <- predict(model1, trainset, type = "class")
+  #table(predTrain, trainset$category_id)
+  
+  predValid <- predict(model1, validset, type = "class")
+  print(mean(predValid == validset$category_id))
+  #table(predValid, validset$category_id)
+}
+  print(test_error)
+  importance(model2)
+  varImpPlot(model2)
+  
+
+
 
